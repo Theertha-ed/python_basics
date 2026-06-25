5eee1a8415fd4ba28a73733244bd3e646d9ef385
Theertha-ed <edtheertha@gmail.com>
2026-06-25 16:52:30 +0530

    python_basics

---
+++ test.py
@@ -0,0 +1,21 @@
+#print("hello world")
+#print("hello world")
+#print("hello world",end=" ")
+#print("hello world"," world",sep=" #")
+#x=5
+#print("hello world","world",sep=" #")
+#x="10.5"
+#print(int(float(x)))
+#print("\test\ntesting")
+#print(r"\test\ntesting")
+#print("\\test\\ntesting")
+#print("""hello
+#        my name is 
+#        john
+#      """)
+#x="hello"
+for i in range(0,10,2):
+    #if i%2==0:
+        print(i)
+    #while True:
+     #   print("hello world")
